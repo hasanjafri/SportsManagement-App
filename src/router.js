cf2fba51c79f64a5f679e4d0ebaf6da2097a7749
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-26 20:14:07 -0500

    Added other components to router file

--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
+import store from "./store.js";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Login from "./components/Login.vue";
+import Register from "./components/Register.vue";
+import Secure from "./components/Secure.vue";
 
 Vue.use(Router);
 
