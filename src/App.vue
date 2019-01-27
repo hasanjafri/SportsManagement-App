887c5e89cfd9a24c9a0e78c47d545a12196380b5
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-26 20:22:03 -0500

    Added expired token case

--- src/App.vue
+++ src/App.vue
@@ -33,8 +33,18 @@
 
 <script>
   export default {
-    computed : {
+    computed: {
       isLoggedIn : function(){ return this.$store.getters.isLoggedIn}
+    },
+    created: function () {
+      this.$http.interceptors.response.use(undefined, function (err) {
+        return new Promise(function (resolve, reject) {
+          if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+            this.$store.dispatch(logout);
+          }
+          throw err;
+        });
+      });
     },
     methods: {
       logout: function () {
