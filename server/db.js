a4ccea0d8f7f876875e7bfadfe210cf16dda7434
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-26 22:29:30 -0500

    Added db functions in db file

--- server/db.js
+++ server/db.js
@@ -21,30 +21,37 @@ class Db {
   selectByEmail(email, callback) {
     return this.db.get(`SELECT * FROM user WHERE email = ?`, [email], function(
       err,
-      row) { callback(err,row) })
+      row
+    ) {
+      callback(err, row);
+    });
   }
 
-    insertAdmin(user, callback) {
-        return this.db.run(
-            'INSERT INTO user (name,email,user_pass,is_admin) VALUES (?,?,?,?)',
-            user, (err) => {
-                callback(err)
-            })
-    }
+  insertAdmin(user, callback) {
+    return this.db.run(
+      "INSERT INTO user (name,email,user_pass,is_admin) VALUES (?,?,?,?)",
+      user,
+      err => {
+        callback(err);
+      }
+    );
+  }
 
-    selectAll(callback) {
-        return this.db.all(`SELECT * FROM user`, function(err,rows){
-            callback(err,rows)
-        })
-    }
+  selectAll(callback) {
+    return this.db.all(`SELECT * FROM user`, function(err, rows) {
+      callback(err, rows);
+    });
+  }
 
-    insert(user, callback) {
-        return this.db.run(
-            'INSERT INTO user (name,email,user_pass) VALUES (?,?,?)',
-            user, (err) => {
-                callback(err)
-            })
-    }
+  insert(user, callback) {
+    return this.db.run(
+      "INSERT INTO user (name,email,user_pass) VALUES (?,?,?)",
+      user,
+      err => {
+        callback(err);
+      }
+    );
+  }
 }
 
-module.exports = Db
+module.exports = Db;
