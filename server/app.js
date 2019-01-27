7a55043d9be8e5b475977f5674305c82249a2699
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-26 21:03:11 -0500

    Added login route to server

--- server/app.js
+++ server/app.js
@@ -62,3 +62,17 @@ router.post("/register-admin", function(req, res) {
     }
   );
 });
+
+router.post("/login", (req, res) => {
+  db.selectByEmail(req.body.email, (err, user) => {
+    if (err) return res.status(500).send("Error on the server.");
+    if (!user) return res.status(404).send("No user found.");
+    let passwordIsValid = bcrypt.compareSync(req.body.password, user.user_pass);
+    if (!passwordIsValid)
+      return res.status(401).send({ auth: false, token: null });
+    let token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+    res.status(200).send({ auth: true, token: token, user: user });
+  });
+});
