0c907d0da8996e735b71eb21eaf4598a48b19694
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-26 20:16:26 -0500

    Updated routes in router file

--- src/router.js
+++ src/router.js
@@ -19,13 +19,27 @@ export default new Router({
       component: Home
     },
     {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "/secure",
+      name: "secure",
+      component: Secure,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
     }
   ]
 });
