88653b0bee9a967d910456bf83d7465037f3f829
Hasan Jafri <hasan.jafri7@gmail.com>
2019-01-28 21:59:16 -0500

    Added new subsection to Home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,92 @@
 <template>
   <v-content>
     <section>
-      <v-parallax :src="require('../assets/hero.jpg')" height="600">
-        <v-layout column align-center justify-center class="white--text">
-          <h1 class="white--text mb-2 display-1 text-xs-center">
-            Welcome to MIC Sports Facilities based in Brampton, ON, Canada.
-          </h1>
-          <div class="subheading mb-3 text-xs-center">
-            This sports portal is created for the community to provide
-            information on sports related activities on a centralized platform
-            offered at the Masumeen Islamic Center.
-          </div>
-          <v-btn class="light-green darken-1 mt-5" dark large href="/login"
-            >Get Started</v-btn
-          >
-        </v-layout>
-      </v-parallax>
+      <v-parallax :src="require('../assets/hero.jpg')" height="400" />
     </section>
 
-    <section></section>
+    <section>
+      <v-layout column wrap class="my-5" align-center>
+        <v-flex xs12 sm4 class="my-3">
+          <div class="text-xs-center">
+            <h2 class="headline">
+              Welcome to MIC Sports Facilities based in Brampton, ON, Canada.
+            </h2>
+            <span class="subheading"
+              >This sports portal is created for the community to provide
+              information on sports related activities on a centralized platform
+              offered at the Masumeen Islamic Center.</span
+            >
+          </div>
+        </v-flex>
+        <v-flex xs12>
+          <v-container grid-list-xl>
+            <v-layout row wrap align-center>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <v-icon x-large class="blue--text text--lighten-2"
+                      >fa-user</v-icon
+                    >
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">
+                      Sports for Everyone!
+                    </div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla
+                    volutpat tincidunt ornare. Pellentesque habitant morbi
+                    tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+                    bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <v-icon x-large class="blue--text text--lighten-2"
+                      >fa-clock</v-icon
+                    >
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline">Updated Daily!</div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla
+                    volutpat tincidunt ornare. Pellentesque habitant morbi
+                    tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+                    bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+              <v-flex xs12 md4>
+                <v-card class="elevation-0 transparent">
+                  <v-card-text class="text-xs-center">
+                    <v-icon x-large class="blue--text text--lighten-2"
+                      >fa-tasks</v-icon
+                    >
+                  </v-card-text>
+                  <v-card-title primary-title class="layout justify-center">
+                    <div class="headline text-xs-center">
+                      Managed by the Community.
+                    </div>
+                  </v-card-title>
+                  <v-card-text>
+                    Cras facilisis mi vitae nunc lobortis pharetra. Nulla
+                    volutpat tincidunt ornare. Pellentesque habitant morbi
+                    tristique senectus et netus et malesuada fames ac turpis
+                    egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus
+                    bibendum tincidunt. Suspendisse potenti.
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </section>
   </v-content>
 </template>
 
