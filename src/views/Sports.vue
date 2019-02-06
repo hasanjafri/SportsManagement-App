fdf3fc6d5b4b31231aca4fe30d6d30282e989d7f
Hasan Jafri <hasan.jafri7@gmail.com>
2019-02-05 23:30:30 -0500

    Moved eeverything back to Sports component & started Register component for each sport:

--- src/views/Sports.vue
+++ src/views/Sports.vue
@@ -1,14 +1,59 @@
 <template>
-  <Sports />
+  <v-content>
+    <section>
+      <Carousel />
+    </section>
+
+    <section class="grey lighten-3 px-5">
+      <About />
+    </section>
+
+    <section class="px-5">
+      <Play />
+    </section>
+
+    <section class="grey lighten-4 px-5">
+      <Join />
+    </section>
+
+    <section class="px-5 py-5 grey lighten-3">
+      <Location />
+    </section>
+
+    <section class="px-5">
+      <UpcomingEvents />
+    </section>
+
+    <section class="px-5 grey lighten-3">
+      <OldEvents />
+    </section>
+
+    <section class="px-5">
+      <Contact />
+    </section>
+  </v-content>
 </template>
 
 <script>
-import Sports from "../components/Sports";
+import About from "../components/sports/About";
+import Carousel from "../components/sports/Carousel";
+import Contact from "../components/sports/Contact";
+import Join from "../components/sports/Join";
+import Location from "../components/sports/Location";
+import OldEvents from "../components/sports/OldEvents";
+import Play from "../components/sports/Play";
+import UpcomingEvents from "../components/sports/UpcomingEvents";
 
 export default {
-  name: "sports",
   components: {
-    Sports
+    About,
+    Carousel,
+    Contact,
+    Join,
+    Location,
+    OldEvents,
+    Play,
+    UpcomingEvents
   }
 };
 </script>
