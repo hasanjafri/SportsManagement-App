671bcd715b141a57964dfc0d2b86626cea7fb78b
Hasan Jafri <hasan.jafri7@gmail.com>
2019-02-07 19:16:11 -0500

    Finished filtering photos by tags feature in PHotoGallery

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -2,12 +2,11 @@
   <div>
     <v-flex xs12 sm6>
       <v-select
-        v-model="value"
         :items="items"
-        chips
+        value="ALL"
         label="FILTER BY YEAR"
-        multiple
         outline
+        v-on:change="filter"
       ></v-select>
     </v-flex>
     <gallery
@@ -17,7 +16,7 @@
     ></gallery>
     <div
       class="image"
-      v-for="(image, imageIndex) in images"
+      v-for="(image, imageIndex) in filteredItems"
       :key="imageIndex"
       @click="index = imageIndex"
       :style="{
@@ -37,23 +36,38 @@ export default {
   data: function() {
     return {
       images: [
-        { src: require("@/assets/img/b1.jpeg"), tag: "2016" },
-        { src: require("@/assets/img/b2.jpeg"), tag: "2016" },
-        { src: require("@/assets/img/b3.jpeg"), tag: "2017" },
-        { src: require("@/assets/img/b4.jpeg"), tag: "2017" },
-        { src: require("@/assets/img/b5.jpeg"), tag: "2018" },
-        { src: require("@/assets/img/b6.jpeg"), tag: "2018" },
-        { src: require("@/assets/img/b7.jpeg"), tag: "2019" },
-        { src: require("@/assets/img/b8.jpeg"), tag: "2019" }
+        { src: require("@/assets/img/b1.jpeg"), tags: ["ALL", "2016"] },
+        { src: require("@/assets/img/b2.jpeg"), tags: ["ALL", "2016"] },
+        { src: require("@/assets/img/b3.jpeg"), tags: ["ALL", "2017"] },
+        { src: require("@/assets/img/b4.jpeg"), tags: ["ALL", "2017"] },
+        { src: require("@/assets/img/b5.jpeg"), tags: ["ALL", "2018"] },
+        { src: require("@/assets/img/b6.jpeg"), tags: ["ALL", "2018"] },
+        { src: require("@/assets/img/b7.jpeg"), tags: ["ALL", "2019"] },
+        { src: require("@/assets/img/b8.jpeg"), tags: ["ALL", "2019"] }
       ],
       index: null,
-      items: ["2016", "2017", "2018", "2019"],
-      value: ["2016", "2017", "2018", "2019"]
+      items: ["ALL", "2016", "2017", "2018", "2019"],
+      currentTag: "ALL"
     };
   },
 
   components: {
     gallery: VueGallery
+  },
+
+  computed: {
+    filteredItems: function() {
+      var filter = this.currentTag;
+      return this.images.filter(function(item) {
+        return item.tags.indexOf(filter) !== -1;
+      });
+    }
+  },
+
+  methods: {
+    filter: function(tag) {
+      this.currentTag = tag;
+    }
   }
 };
 </script>
